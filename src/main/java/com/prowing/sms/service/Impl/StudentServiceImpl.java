829fe938877c7c436c77c07b397e742232b07f4c
RahulK9495 <rahulkhot5554@gmail.com>
2024-06-26 20:24:38 +0530

    new

--- src/main/java/com/prowing/sms/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/prowing/sms/service/Impl/StudentServiceImpl.java
@@ -7,32 +7,29 @@ import org.springframework.stereotype.Service;
 import com.prowing.sms.entity.Student;
 import com.prowing.sms.repository.StudentRepository;
 import com.prowing.sms.service.StudentService;
+
 @Service
-public class StudentServiceImpl implements StudentService{
+public class StudentServiceImpl implements StudentService {
 
 	private StudentRepository repository;
-	
-	
+
 	public StudentServiceImpl(StudentRepository repository) {
 		super();
 		this.repository = repository;
 	}
 
-
 	@Override
 	public List<Student> getAllStudents() {
 
 		return repository.findAll();
-		
-	}
 
+	}
 
 	@Override
 	public Student saveStudent(Student student) {
-		
+
 		return repository.save(student);
 	}
-
 
 	@Override
 	public Student getStudentById(Long id) {
@@ -40,17 +37,15 @@ public class StudentServiceImpl implements StudentService{
 		return repository.findById(id).get();
 	}
 
-
 	@Override
 	public Student updateStudent(Student student) {
-	
+
 		return repository.save(student);
 	}
 
-
 	@Override
 	public void deleteStudent(Long id) {
-		
+
 		repository.deleteById(id);
 	}
 
